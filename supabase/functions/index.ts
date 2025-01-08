7e61e6972a3a6339ff6d80e91c7a390ce16dffb8
Timonwa Akintokun <timonwaakintokun@gmail.com>
2025-01-08 12:16:36 +0100

    Add Permitio integration for permission management in Supabase functions

---
+++ supabase/functions/index.ts
@@ -0,0 +1,7 @@
+import { Permit } from "npm:permitio";
+
+export const permit = new Permit({
+    token:
+        "permit_key_CIsSsMX0KQcIZDnVZr1R23pWkNQPp6ngjibiyMCpBKA1haiNmEf4ctjvdese5eg75LCCHaRbfYpk6c4J05yBOY",
+    pdp: "<http://localhost:7766>",
+});
